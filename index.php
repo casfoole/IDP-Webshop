df05dff68552342f4ef5b54a10fc7e54fff9f0fe
Cas Foole <22937@ma-web.nl>
2018-02-07 14:08:48 +0100

    new update
    products page

--- index.php
+++ index.php
@@ -6,16 +6,82 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
     <title>New in</title>
 </head>
 <body>
     <div class="row">
+        <div class="banner crop">
+            <img src="images/stoneislandlead.jpg" alt="Banner" width="1280">
+        </div>
         <div class="clothing-row">
-            <div class="product"></div>
-            <div class="product"></div>
-            <div class="product"></div>
-            <div class="product"></div>
+            <div class="product" id="product">
+                <img id="img" src="product-images/jacket.jpg" alt="jacket">
+                <p class="product-title">41322 MICRO REPS</p>
+                <div class="sizes">
+                    <a class="large margin-sizes">XS</a>
+                    <a class="small margin-sizes">S</a>
+                    <a class="medium margin-sizes">M</a>
+                    <a class="large margin-sizes">L</a>
+                    <a class="large margin-sizes">XL</a>
+                </div>
+                <div class="available-colors">
+                    <div class="color color-blue" onclick="document.getElementById('img').src='product-images/jacketb.jpg'"></div>
+                    <div class="color color-yellow" onclick="document.getElementById('img').src='product-images/jacket.jpg'"></div>
+                    <div class="color color-black" onclick="document.getElementById('img').src='product-images/jacketc.jpg'"></div>
+                    <div class="color color-white" onclick="document.getElementById('img').src='product-images/jacketd.jpg'"></div>
+                </div>
+            </div>
+            <div class="product">
+                <img id="img" src="" alt="">
+                <div class="sizes">
+                    <a class="large margin-sizes">XS</a>
+                    <a class="small margin-sizes">S</a>
+                    <a class="medium margin-sizes">M</a>
+                    <a class="large margin-sizes">L</a>
+                    <a class="large margin-sizes">XL</a>
+                </div>
+                <div class="available-colors">
+                    <div class="color color-blue"></div>
+                    <div class="color color-yellow"></div>
+                    <div class="color color-black"></div>
+                    <div class="color color-white"></div>
+                </div>
+            </div>
+            <div class="product">
+                <img id="img" src="" alt="">
+                <div class="sizes">
+                    <a class="large margin-sizes">XS</a>
+                    <a class="small margin-sizes">S</a>
+                    <a class="medium margin-sizes">M</a>
+                    <a class="large margin-sizes">L</a>
+                    <a class="large margin-sizes">XL</a>
+                </div>
+                <div class="available-colors">
+                    <div class="color color-blue"></div>
+                    <div class="color color-yellow"></div>
+                    <div class="color color-black"></div>
+                    <div class="color color-white"></div>
+                </div>
+            </div>
+            <div class="product">
+                <img id="img" src="" alt="">
+                <div class="sizes">
+                    <a class="large margin-sizes">XS</a>
+                    <a class="small margin-sizes">S</a>
+                    <a class="medium margin-sizes">M</a>
+                    <a class="large margin-sizes">L</a>
+                    <a class="large margin-sizes">XL</a>
+                </div>
+                <div class="available-colors">
+                    <div class="color color-blue"></div>
+                    <div class="color color-yellow"></div>
+                    <div class="color color-black"></div>
+                    <div class="color color-white"></div>
+                </div>
+            </div>
         </div>
     </div>
+<footer></footer>
 </body>
 </html>
